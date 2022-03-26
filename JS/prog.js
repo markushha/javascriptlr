17b7e9a7d450dfbcbb220c0c7a66f12cb4cc6564
Mark <ingerkrut@gmail.com>
2022-03-26 23:51:45 +0600

    first commit

---
+++ JS/prog.js
@@ -0,0 +1,18 @@
+const age = +prompt('Ваш возраст?', '');
+// const n2 = +prompt('number 2?', '');
+
+/* function calc() {
+    document.write(n1 * n2)
+}
+
+calc() */
+
+
+if (age >= 18) {
+    document.write('Welcome !!!');
+}
+
+else if (age < 18) {
+    document.write("Sorry, you are too young :(")
+} 
+
---
+++ JS/prog2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const userName = prompt('Enter your name');
+const userPassword = prompt('Enter your password');
+
+console.log(userName == 'Mark');
+console.log(userPassword == '123mark');
